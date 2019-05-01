7c70ed066f43838402089fdd4f50e640253554e0
Grace Milton <gmmilton10@gmail.com>
2019-05-01 13:38:43 -0400

    pls

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_templates
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+#    return "Hello World!"
